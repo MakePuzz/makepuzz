4495153295f17845b491d567dfe46b8f12865ac5
Nishijima <saikorokorokoro20072007@gmail.com>
2021-01-24 06:00:10 +0900

    publicPath修正

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   outputDir: 'docs',
-  publicPath: '/makepuzz',
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/makepuzz/'
+      : '/',
   filenameHashing: false,
   productionSourceMap: false,
 }
